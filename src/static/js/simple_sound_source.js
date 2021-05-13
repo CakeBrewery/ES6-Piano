ce7fa8c3f16e4a159bc59809fb33ddbc625bc9c6
Samuel Navarrete <samueln@uvic.ca>
2021-05-12 22:22:00 -0700

    6) Implement compressor in SimpleSoundSource

--- src/static/js/simple_sound_source.js
+++ src/static/js/simple_sound_source.js
@@ -3,10 +3,32 @@ import { SoundSource } from './sound_source.js'
 export class SimpleSoundSource extends SoundSource {
     constructor() {
         super();
-        this.volume = 0.7;  // 70 %
+        this.audioContext = new (window.AudioContext || window.webkitAudioContext);
+
+        this.volume = this.audioContext.createGain();
+        this.volume.gain.value = 0.7;  // 70%
+        this.volume.connect(this.audioContext.destination);
+
+        // Make sure signal doesn't clip. 
+        this.compressor = this.audioContext.createDynamicsCompressor();
+        this.compressor.ratio.value = 15;
+        this.compressor.threshold.value = -20;
+        this.compressor.connect(this.volume);
     }
 
-    playFrequency(frequency) {
-        console.log('Playing frequency', frequency, 'with volume', this.volume);
+    playFrequency(frequency, volume = 1) {
+        console.log('Playing frequency', frequency, 'with volume', volume);
+
+        let noteVolume = this.audioContext.createGain();
+        noteVolume.gain.value = volume;
+        noteVolume.connect(this.compressor);
+
+        let oscillator = this.audioContext.createOscillator();
+        oscillator.type = 'square';
+        oscillator.frequency.value = frequency;
+        oscillator.connect(noteVolume);
+
+        oscillator.start();
+        oscillator.stop(this.audioContext.currentTime + 1);  // 1 second.
     }
 }
